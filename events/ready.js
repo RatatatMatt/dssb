f9d4e3e227d7d005ed95a70cd56684342feed348
RatatatMatt <65262102+RatatatMatt@users.noreply.github.com>
2020-12-15 15:21:39 -0500

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,8 @@
-h
+module.exports = bot => {
+    bot.users.fetch("323504127449759746").then(matt => { 
+        matt.send(`${bot.user.username} is online!`);
+    });
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity("over the Dictionary Support Server", {type: "WATCHING"});
+    
+}
